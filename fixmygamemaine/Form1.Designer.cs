7e590206a10a9784a3672ee5a84998af65a3e313
mojobojo <mojobojo@live.com>
2020-08-04 10:09:29 -0400

    Added the ability to kill everything.

--- fixmygamemaine/Form1.Designer.cs
+++ fixmygamemaine/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.z_text = new System.Windows.Forms.Label();
             this.hp_text = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,20 +43,19 @@
             this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Location = new System.Drawing.Point(12, 194);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(475, 524);
+            this.listBox1.Size = new System.Drawing.Size(475, 498);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox2.Location = new System.Drawing.Point(502, 12);
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox2.Location = new System.Drawing.Point(105, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(596, 174);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.Size = new System.Drawing.Size(288, 174);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
@@ -65,9 +65,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(502, 192);
+            this.listBox2.Location = new System.Drawing.Point(502, 10);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(431, 342);
+            this.listBox2.Size = new System.Drawing.Size(431, 680);
             this.listBox2.TabIndex = 3;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
@@ -75,7 +75,7 @@
             // 
             this.yaw_text.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.yaw_text.AutoSize = true;
-            this.yaw_text.Location = new System.Drawing.Point(954, 223);
+            this.yaw_text.Location = new System.Drawing.Point(951, 12);
             this.yaw_text.Name = "yaw_text";
             this.yaw_text.Size = new System.Drawing.Size(31, 13);
             this.yaw_text.TabIndex = 4;
@@ -85,7 +85,7 @@
             // 
             this.pitch_text.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pitch_text.AutoSize = true;
-            this.pitch_text.Location = new System.Drawing.Point(954, 246);
+            this.pitch_text.Location = new System.Drawing.Point(951, 35);
             this.pitch_text.Name = "pitch_text";
             this.pitch_text.Size = new System.Drawing.Size(34, 13);
             this.pitch_text.TabIndex = 5;
@@ -95,7 +95,7 @@
             // 
             this.roll_text.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.roll_text.AutoSize = true;
-            this.roll_text.Location = new System.Drawing.Point(954, 269);
+            this.roll_text.Location = new System.Drawing.Point(951, 58);
             this.roll_text.Name = "roll_text";
             this.roll_text.Size = new System.Drawing.Size(28, 13);
             this.roll_text.TabIndex = 6;
@@ -105,7 +105,7 @@
             // 
             this.x_text.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.x_text.AutoSize = true;
-            this.x_text.Location = new System.Drawing.Point(954, 293);
+            this.x_text.Location = new System.Drawing.Point(951, 82);
             this.x_text.Name = "x_text";
             this.x_text.Size = new System.Drawing.Size(17, 13);
             this.x_text.TabIndex = 7;
@@ -115,7 +115,7 @@
             // 
             this.y_text.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.y_text.AutoSize = true;
-            this.y_text.Location = new System.Drawing.Point(954, 315);
+            this.y_text.Location = new System.Drawing.Point(951, 104);
             this.y_text.Name = "y_text";
             this.y_text.Size = new System.Drawing.Size(17, 13);
             this.y_text.TabIndex = 8;
@@ -125,7 +125,7 @@
             // 
             this.z_text.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.z_text.AutoSize = true;
-            this.z_text.Location = new System.Drawing.Point(954, 337);
+            this.z_text.Location = new System.Drawing.Point(951, 126);
             this.z_text.Name = "z_text";
             this.z_text.Size = new System.Drawing.Size(17, 13);
             this.z_text.TabIndex = 9;
@@ -135,7 +135,7 @@
             // 
             this.hp_text.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.hp_text.AutoSize = true;
-            this.hp_text.Location = new System.Drawing.Point(954, 358);
+            this.hp_text.Location = new System.Drawing.Point(951, 147);
             this.hp_text.Name = "hp_text";
             this.hp_text.Size = new System.Drawing.Size(25, 13);
             this.hp_text.TabIndex = 10;
@@ -143,7 +143,8 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(976, 397);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(982, 638);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 11;
@@ -151,11 +152,23 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(982, 667);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Kill All";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1110, 548);
+            this.ClientSize = new System.Drawing.Size(1110, 704);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.hp_text);
             this.Controls.Add(this.z_text);
@@ -189,6 +202,7 @@
         private System.Windows.Forms.Label z_text;
         private System.Windows.Forms.Label hp_text;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ fixmygamemaine/MaineSave.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace fixmygamemaine {
+    public struct maine_actor_data {
+        public int file_offset;
+        public string bp_name;
+        public float yaw;
+        public float pitch;
+        public float roll;
+        public float x;
+        public float y;
+        public float z;
+        public int hp_position;
+        public float hp;
+    }
+    public class MaineSave {
+
+        public Image screenshot;
+        public List<maine_actor_data> actors;
+
+        private BinaryReader binary_reader;
+        private BinaryWriter binary_writer;
+        private FileStream file_stream;
+
+        public static string[] get_save_list(string save_path) {
+            string[] saves = Directory.GetDirectories(save_path);
+            return saves;
+        }
+
+        public static string[] get_save_list() {
+            string save_path = Environment.GetEnvironmentVariable("USERPROFILE") + "\\Saved Games\\Grounded\\";
+            return get_save_list(save_path);
+        }
+
+        public void open(string folder_path) {
+            string save_path = folder_path + "\\World.sav";
+            string save_image = folder_path + "\\SaveGameScreenshot.png";
+
+            if (File.Exists(save_image)) {
+                screenshot = Image.FromFile(save_image);
+            }
+
+            if (File.Exists(save_path)) {
+                actors = new List<maine_actor_data>();
+
+                file_stream = new FileStream(save_path, FileMode.Open);
+                binary_reader = new BinaryReader(file_stream);
+                binary_writer = new BinaryWriter(file_stream);
+
+                binary_reader.BaseStream.Position = 0x10;
+
+                int item_count = binary_reader.ReadInt32();
+
+                for (int i = 0; i < item_count; i++) {
+                    int data_size = binary_reader.ReadInt32();
+                    int next_offset = (int)binary_reader.BaseStream.Position + data_size - 4;
+
+                    binary_reader.BaseStream.Position += 0x14;
+                    int bp_size = binary_reader.ReadInt32();
+                    string bp = Encoding.ASCII.GetString(binary_reader.ReadBytes(bp_size - 1));
+
+                    if (bp == "/Game/Blueprints/Global/BP_SurvivalGameState.BP_SurvivalGameState_C") {
+
+                        binary_reader.BaseStream.Position += 0x68;
+                        int actor_count = binary_reader.ReadInt32();
+
+                        for (int ac = 0; ac < actor_count; ac++) {
+                            maine_actor_data a_data = new maine_actor_data();
+
+                            a_data.file_offset = (int)binary_reader.BaseStream.Position;
+
+                            int size = binary_reader.ReadInt32();
+                            int next_actor_offset = (int)binary_reader.BaseStream.Position + size - 4;
+
+                            binary_reader.BaseStream.Position += 0xC;
+
+                            int actor_type_len = binary_reader.ReadInt32();
+                            string actor_type_str = Encoding.ASCII.GetString(binary_reader.ReadBytes(actor_type_len - 1));
+
+                            binary_reader.BaseStream.Position += 1;
+
+                            int actor_str_size = binary_reader.ReadInt32();
+                            string actor_str = Encoding.ASCII.GetString(binary_reader.ReadBytes(actor_str_size - 1));
+
+                            binary_reader.BaseStream.Position += 1;
+
+                            float dummy = binary_reader.ReadSingle();
+
+                            a_data.yaw = binary_reader.ReadSingle();
+                            a_data.pitch = binary_reader.ReadSingle();
+                            a_data.roll = binary_reader.ReadSingle();
+
+                            a_data.x = binary_reader.ReadSingle();
+                            a_data.y = binary_reader.ReadSingle();
+                            a_data.z = binary_reader.ReadSingle();
+
+                            dummy = binary_reader.ReadSingle();
+                            dummy = binary_reader.ReadSingle();
+                            dummy = binary_reader.ReadSingle();
+                            dummy = binary_reader.ReadSingle();
+                            dummy = binary_reader.ReadSingle();
+                            dummy = binary_reader.ReadSingle();
+                            dummy = binary_reader.ReadSingle();
+
+                            int some_int_0 = binary_reader.ReadInt32();
+                            int some_int_1 = binary_reader.ReadInt32();
+
+                            if (some_int_1 == 0) {
+                                int actor_property_count = binary_reader.ReadInt32();
+
+                                for (int apc = 0; apc < actor_property_count; apc++) {
+                                    int ap_size = binary_reader.ReadInt32();
+                                    int next_ap = (int)binary_reader.BaseStream.Position + ap_size - 4;
+
+                                    binary_reader.BaseStream.Position += 0xC;
+                                    int ap_str_size = binary_reader.ReadInt32();
+                                    string ap_str = Encoding.ASCII.GetString(binary_reader.ReadBytes(ap_str_size - 1));
+
+                                    if (ap_str == "/Script/Maine.HealthLODComponent") {
+                                        binary_reader.BaseStream.Position += 1;
+                                        a_data.hp_position = (int)binary_reader.BaseStream.Position;
+                                        a_data.hp = binary_reader.ReadSingle();
+                                    }
+
+                                    binary_reader.BaseStream.Position = next_ap;
+                                }
+                            }
+
+                            binary_reader.BaseStream.Position = next_actor_offset;
+
+                            a_data.bp_name = actor_str;
+
+                            actors.Add(a_data);
+                        }
+
+                        break;
+                    }
+
+                    binary_reader.BaseStream.Position = next_offset;
+                }
+            }
+        }
+
+        public void kill_larva() {
+            foreach (maine_actor_data ad in actors) {
+                if (ad.bp_name == "/Game/Blueprints/Creatures/Larva/Base/BP_Larva.BP_Larva_C") {
+                    binary_writer.BaseStream.Position = ad.hp_position;
+                    binary_writer.Write((float)0.0f);
+                }
+
+                binary_writer.Flush();
+            }
+        }
+
+        public void kill_everything() {
+            foreach (maine_actor_data ad in actors) {
+                if (ad.hp > 0) {
+                    binary_writer.BaseStream.Position = ad.hp_position;
+                    binary_writer.Write((float)0.0f);
+                }
+
+                binary_writer.Flush();
+            }
+        }
+
+        public void close() {
+            if (binary_writer != null) {
+                binary_reader.Close();
+            }
+
+            if (binary_writer != null) {
+                binary_writer.Close();
+            }
+
+            if (file_stream != null) {
+                file_stream.Close();
+            }
+        }
+    }
+}
